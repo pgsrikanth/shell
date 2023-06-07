cc17b59ad879eee70bec806f9490bd85274c5288
srikanth <gowtham@1996.com>
2023-06-07 19:19:31 +0530

    script

--- myshell.sh
+++ myshell.sh
@@ -1,21 +1,21 @@
 
 logo=/tmp/out
-ram=sudo
+
 
 echo updating system
-ram apt-get update       &>>logo
+ apt-get update       &>>logo
 echo status =$?
 
 echo installing nginx
-ram apt-get install nginx &>>logo
+ apt-get install nginx &>>logo
 echo status =$?
 
 echo nginx staring
-ram service nginx start
+ service nginx start
 echo status =$?
 
 echo change directoty
-ram cd /opt       &>>logo
+ cd /opt       &>>logo
 
 echo creating folder
 mkdir gow     &>>logo
