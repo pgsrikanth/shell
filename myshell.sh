03382c223d2c87cf6bbd6fc3a371d0c9442847eb
srikanth <gowtham@1996.com>
2023-06-07 19:10:23 +0530

    script

--- myshell.sh
+++ myshell.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
-ram=sudo
+
 logo=/tmp/out
 
 echo updating system
-ram apt-get update       &>>logo
+sudo apt-get update       &>>logo
 echo status =$?
 
 echo installing nginx
-ram apt-get install nginx  &>>logo
+sudo apt-get install nginx  &>>logo
 echo status =$?
 
 echo nginx staring
-ram service nginx start
+sudo service nginx start
 echo status =$?
 
 echo creating folder
-ram cd /opt       &>>logo
+sudo cd /opt       &>>logo
 mkdir gow     &>>logo
