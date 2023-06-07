ab15164b580dcc42f9aad64b120ba92dc5eaedfc
srikanth <gowtham@1996.com>
2023-06-07 19:15:01 +0530

    script

--- myshell.sh
+++ myshell.sh
@@ -6,7 +6,7 @@ sudo apt-get update       &>>logo
 echo status =$?
 
 echo installing nginx
-sudo apt-get install nginx -y &>>logo
+sudo apt-get install ngin &>>logo
 echo status =$?
 
 echo nginx staring
